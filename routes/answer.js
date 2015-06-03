026713f66032e005a5c571b6fc53efeb437d3f09
Armando Urdiales <aurgo@outlook.com>
2015-06-03 22:06:20 +0200

    Vistas parciales
    Vistas parciales

--- routes/answer.js
+++ routes/answer.js
@@ -4,7 +4,7 @@ var router = express.Router();
 //var quizController = require('../controllers/quiz_controller');
 
 router.get('/', function(req, res, next) {
-  var mensaje = (req.query.respuesta.toUpperCase() == "ROMA" ? "Respuesta correcta" : "Respuesta incorrecta");
+  var mensaje = (req.query.respuesta.toUpperCase() == "ROMA" ? "Correcto" : "Incorrecto");
 
   res.render('quizes/answer.ejs', { title: 'Quiz' , resultado: mensaje });
 });
